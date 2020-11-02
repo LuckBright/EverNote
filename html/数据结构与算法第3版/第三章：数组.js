503584a22121c915b47daeea26be2f3cfce68036
张晟 <2402536045@qq.com>
2020-11-03 06:43:44 +0800

    数组

--- html/数据结构与算法第3版/第三章：数组.js
+++ html/数据结构与算法第3版/第三章：数组.js
@@ -1,7 +1,7 @@
 /*
  * @Author: by zhangsheng@everjiankang.com.cn
  * @Date: 2020-11-01 06:33:38
- * @LastEditTime: 2020-11-01 07:06:14
+ * @LastEditTime: 2020-11-03 06:29:56
  * @LastEditors: by zhangsheng@everjiankang.com.cn
  */
 /**
@@ -35,12 +35,45 @@
 // 首先需要腾出数组里的第一个元素的位置，把所有的元素向右移一位。可以循环数组中的元素，从最后一位开始，将对应的上一个元素 （i - 1）的值赋值给它（i），
 // 依次处理，最后把想要的值赋值给第一个位置（索引 0 ）
 // 跟 unshift 的执行是一样的
-Array.prototype.insertFirstPosition = function (value) {
-  for (let i = this.length; i >= 0; i--) {
-    this[i] = this[i - 1]
+// Array.prototype.insertFirstPosition = function (value) {
+//   for (let i = this.length; i >= 0; i--) {
+//     this[i] = this[i - 1]
+//   }
+//   this[0] = value
+// }
+// let ary = [1, 2, 3, 4, 5]
+// ary.insertFirstPosition(6)
+// console.log(ary) // [6, 1, 2, 3, 4, 5]
+
+/**
+ * 移除数组的第一个元素
+ * 元素整体右移一位，创建一个新数组，将不是 undefined 的元素 push 进新数组
+ */
+
+Array.prototype.reIndex = function (myArray) {
+  const newArr = []
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] !== undefined) newArr.push(myArray[i])
   }
-  this[0] = value
+  console.log(newArr)
+  return newArr
 }
-let ary = [1, 2, 3, 4, 5]
-ary.insertFirstPosition(6)
-console.log(ary) // [6, 1, 2, 3, 4, 5]
+
+Array.prototype.ZsPop = function () {
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i + 1]
+  }
+  return this.reIndex(this)
+}
+
+/**
+ * 二维数组
+ * console.table(Array) 打印一个表单样式的数组，更好的理解数组的每一项
+ */
+
+ let twoAry = [
+   [1, 2, 3],
+   [4, 5, 6]
+ ]
+ console.table(twoAry)
+
